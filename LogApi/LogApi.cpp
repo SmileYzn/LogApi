01a2d4d8f2f9e536c61123cc15c33100fd03de32
Cleverson <SmileYzn@users.noreply.github.com>
2024-09-24 13:25:58 -0300

    Update LogApi.cpp

--- LogApi/LogApi.cpp
+++ LogApi/LogApi.cpp
@@ -27,12 +27,6 @@ void CLogApi::ServerActivate()
 	// Reset next frame time
 	this->m_FrameTime = (gpGlobals->time + this->m_log_api_delay->value);
 
-	// Clear events
-	if (!this->m_Events.empty())
-	{
-		this->m_Events.clear();
-	}
-	
 	try
 	{
 		// File stream
