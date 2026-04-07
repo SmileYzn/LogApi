eed56e9c7be5fe7f4ec6179a53d681d49d94cce5
Celio Lozatto <lozatto@outlook.com>
2026-04-07 15:59:24 -0300

    windows correction

--- LogApi/MetaMod.cpp
+++ LogApi/MetaMod.cpp
@@ -25,7 +25,17 @@ gamedll_funcs_t *gpGamedllFuncs;
 mutil_funcs_t *gpMetaUtilFuncs;
 META_FUNCTIONS gMetaFunctionTable;
 
-// ✅ Function Declarations (Required for Windows Linker)
+// ✅ FORWARD DECLARATIONS (Fixes LNK2001)
+void DLL_POST_ServerActivate(edict_t *pEdictList, int edictCount, int clientMax);
+void DLL_POST_ServerDeactivate(void);
+void DLL_POST_StartFrame(void);
+qboolean DLL_POST_ClientConnect(edict_t *pEntity, const char *pszName, const char *pszAddress, char szRejectReason[128]);
+void DLL_POST_ClientPutInServer(edict_t *pEntity);
+void DLL_POST_ClientKill(edict_t *pEntity);
+void DLL_POST_ClientUserInfoChanged(edict_t *pEntity, char *InfoBuffer);
+void DLL_POST_ClientCommand(edict_t *pEntity);
+void ENGINE_POST_AlertMessage(ALERT_TYPE atype, const char *szFmt, ...);
+
 C_DLLEXPORT int GetEntityAPI2(DLL_FUNCTIONS *pFunctionTable, int *interfaceVersion);
 C_DLLEXPORT int GetEntityAPI2_Post(DLL_FUNCTIONS *pFunctionTable, int *interfaceVersion);
 C_DLLEXPORT int GetEngineFunctions(enginefuncs_t *pengfuncsFromEngine, int *interfaceVersion);
@@ -33,58 +43,13 @@ C_DLLEXPORT int GetEngineFunctions_Post(enginefuncs_t *pengfuncsFromEngine, int 
 C_DLLEXPORT int GetNewDLLFunctions(NEW_DLL_FUNCTIONS *pNewFunctionTable, int *interfaceVersion);
 C_DLLEXPORT int GetNewDLLFunctions_Post(NEW_DLL_FUNCTIONS *pNewFunctionTable, int *interfaceVersion);
 
-// ✅ Function Definitions
+// Global Function Tables
 DLL_FUNCTIONS g_DLL_FunctionTable_Pre;
 DLL_FUNCTIONS g_DLL_FunctionTable_Post;
 NEW_DLL_FUNCTIONS g_DLL_NewFunctionTable_Pre;
 NEW_DLL_FUNCTIONS g_DLL_NewFunctionTable_Post;
 enginefuncs_t g_ENGINE_FunctionTable_Pre;
 enginefuncs_t g_ENGINE_FunctionTable_Post;
-
-C_DLLEXPORT int GetEntityAPI2(DLL_FUNCTIONS *pFunctionTable, int *interfaceVersion) {
-    memset(&g_DLL_FunctionTable_Pre, 0, sizeof(DLL_FUNCTIONS));
-    memcpy(pFunctionTable, &g_DLL_FunctionTable_Pre, sizeof(DLL_FUNCTIONS));
-    return 1;
-}
-
-C_DLLEXPORT int GetEntityAPI2_Post(DLL_FUNCTIONS *pFunctionTable, int *interfaceVersion) {
-    memset(&g_DLL_FunctionTable_Post, 0, sizeof(DLL_FUNCTIONS));
-    g_DLL_FunctionTable_Post.pfnServerActivate = DLL_POST_ServerActivate;
-    g_DLL_FunctionTable_Post.pfnServerDeactivate = DLL_POST_ServerDeactivate;
-    g_DLL_FunctionTable_Post.pfnStartFrame = DLL_POST_StartFrame;
-    g_DLL_FunctionTable_Post.pfnClientConnect = DLL_POST_ClientConnect;
-    g_DLL_FunctionTable_Post.pfnClientPutInServer = DLL_POST_ClientPutInServer;
-    g_DLL_FunctionTable_Post.pfnClientKill = DLL_POST_ClientKill;
-    g_DLL_FunctionTable_Post.pfnClientUserInfoChanged = DLL_POST_ClientUserInfoChanged;
-    g_DLL_FunctionTable_Post.pfnClientCommand = DLL_POST_ClientCommand;
-    memcpy(pFunctionTable, &g_DLL_FunctionTable_Post, sizeof(DLL_FUNCTIONS));
-    return 1;
-}
-
-C_DLLEXPORT int GetEngineFunctions(enginefuncs_t *pengfuncsFromEngine, int *interfaceVersion) {
-    memset(&g_ENGINE_FunctionTable_Pre, 0, sizeof(enginefuncs_t));
-    memcpy(pengfuncsFromEngine, &g_ENGINE_FunctionTable_Pre, sizeof(enginefuncs_t));
-    return 1;
-}
-
-C_DLLEXPORT int GetEngineFunctions_Post(enginefuncs_t *pengfuncsFromEngine, int *interfaceVersion) {
-    memset(&g_ENGINE_FunctionTable_Post, 0, sizeof(enginefuncs_t));
-    g_ENGINE_FunctionTable_Post.pfnAlertMessage = ENGINE_POST_AlertMessage;
-    memcpy(pengfuncsFromEngine, &g_ENGINE_FunctionTable_Post, sizeof(enginefuncs_t));
-    return 1;
-}
-
-C_DLLEXPORT int GetNewDLLFunctions(NEW_DLL_FUNCTIONS *pNewFunctionTable, int *interfaceVersion) {
-    memset(&g_DLL_NewFunctionTable_Pre, 0, sizeof(NEW_DLL_FUNCTIONS));
-    memcpy(pNewFunctionTable, &g_DLL_NewFunctionTable_Pre, sizeof(NEW_DLL_FUNCTIONS));
-    return 1;
-}
-
-C_DLLEXPORT int GetNewDLLFunctions_Post(NEW_DLL_FUNCTIONS *pNewFunctionTable, int *interfaceVersion) {
-    memset(&g_DLL_NewFunctionTable_Post, 0, sizeof(NEW_DLL_FUNCTIONS));
-    memcpy(pNewFunctionTable, &g_DLL_NewFunctionTable_Post, sizeof(NEW_DLL_FUNCTIONS));
-    return 1;
-}
 
 // ✅ GiveFnptrsToDll
 C_DLLEXPORT void WINAPI GiveFnptrsToDll(enginefuncs_t *pengfuncsFromEngine, globalvars_t *pGlobals) {
@@ -142,17 +107,126 @@ C_DLLEXPORT int Meta_Query(char *interfaceVersion, plugin_info_t **pPlugInfo,
     return TRUE;
 }
 
+// ✅ GetEntityAPI2
+C_DLLEXPORT int GetEntityAPI2(DLL_FUNCTIONS *pFunctionTable, int *interfaceVersion) {
+    memset(&g_DLL_FunctionTable_Pre, 0, sizeof(DLL_FUNCTIONS));
+    memcpy(pFunctionTable, &g_DLL_FunctionTable_Pre, sizeof(DLL_FUNCTIONS));
+    return 1;
+}
+
+// ✅ GetEntityAPI2_Post
+C_DLLEXPORT int GetEntityAPI2_Post(DLL_FUNCTIONS *pFunctionTable, int *interfaceVersion) {
+    memset(&g_DLL_FunctionTable_Post, 0, sizeof(DLL_FUNCTIONS));
+    g_DLL_FunctionTable_Post.pfnServerActivate = DLL_POST_ServerActivate;
+    g_DLL_FunctionTable_Post.pfnServerDeactivate = DLL_POST_ServerDeactivate;
+    g_DLL_FunctionTable_Post.pfnStartFrame = DLL_POST_StartFrame;
+    g_DLL_FunctionTable_Post.pfnClientConnect = DLL_POST_ClientConnect;
+    g_DLL_FunctionTable_Post.pfnClientPutInServer = DLL_POST_ClientPutInServer;
+    g_DLL_FunctionTable_Post.pfnClientKill = DLL_POST_ClientKill;
+    g_DLL_FunctionTable_Post.pfnClientUserInfoChanged = DLL_POST_ClientUserInfoChanged;
+    g_DLL_FunctionTable_Post.pfnClientCommand = DLL_POST_ClientCommand;
+    memcpy(pFunctionTable, &g_DLL_FunctionTable_Post, sizeof(DLL_FUNCTIONS));
+    return 1;
+}
+
+// ✅ GetEngineFunctions
+C_DLLEXPORT int GetEngineFunctions(enginefuncs_t *pengfuncsFromEngine, int *interfaceVersion) {
+    memset(&g_ENGINE_FunctionTable_Pre, 0, sizeof(enginefuncs_t));
+    memcpy(pengfuncsFromEngine, &g_ENGINE_FunctionTable_Pre, sizeof(enginefuncs_t));
+    return 1;
+}
+
+// ✅ GetEngineFunctions_Post
+C_DLLEXPORT int GetEngineFunctions_Post(enginefuncs_t *pengfuncsFromEngine, int *interfaceVersion) {
+    memset(&g_ENGINE_FunctionTable_Post, 0, sizeof(enginefuncs_t));
+    g_ENGINE_FunctionTable_Post.pfnAlertMessage = ENGINE_POST_AlertMessage;
+    memcpy(pengfuncsFromEngine, &g_ENGINE_FunctionTable_Post, sizeof(enginefuncs_t));
+    return 1;
+}
+
+// ✅ GetNewDLLFunctions
+C_DLLEXPORT int GetNewDLLFunctions(NEW_DLL_FUNCTIONS *pNewFunctionTable, int *interfaceVersion) {
+    memset(&g_DLL_NewFunctionTable_Pre, 0, sizeof(NEW_DLL_FUNCTIONS));
+    memcpy(pNewFunctionTable, &g_DLL_NewFunctionTable_Pre, sizeof(NEW_DLL_FUNCTIONS));
+    return 1;
+}
+
+// ✅ GetNewDLLFunctions_Post
+C_DLLEXPORT int GetNewDLLFunctions_Post(NEW_DLL_FUNCTIONS *pNewFunctionTable, int *interfaceVersion) {
+    memset(&g_DLL_NewFunctionTable_Post, 0, sizeof(NEW_DLL_FUNCTIONS));
+    memcpy(pNewFunctionTable, &g_DLL_NewFunctionTable_Post, sizeof(NEW_DLL_FUNCTIONS));
+    return 1;
+}
+
+// ✅ DLL_POST Function Definitions
+void DLL_POST_ServerActivate(edict_t *pEdictList, int edictCount, int clientMax) {
+    gLogCvar.ServerActivate();
+    if (g_pLogApi) gLogApi.ServerActivate();
+    if (g_pLogCurl) gLogCurl.ServerActivate();
+    gLogCommand.ServerActivate();
+    gLogEvent.ServerActivate(pEdictList, edictCount, clientMax);
+    RETURN_META(MRES_IGNORED);
+}
+
+void DLL_POST_ServerDeactivate(void) {
+    if (g_pLogApi) gLogApi.ServerDeactivate();
+    gLogEvent.ServerDeactivate();
+    RETURN_META(MRES_IGNORED);
+}
+
+void DLL_POST_StartFrame() {
+    if (g_pLogCurl) gLogCurl.ServerFrame();
+    if (g_pLogApi) gLogApi.ServerFrame();
+    RETURN_META(MRES_IGNORED);
+}
+
+qboolean DLL_POST_ClientConnect(edict_t *pEntity, const char *pszName, const char *pszAddress, char szRejectReason[128]) {
+    gLogPlayer.Connect(pEntity, pszName, pszAddress);
+    gLogEvent.ClientConnect(pEntity, pszName, pszAddress, szRejectReason);
+    RETURN_META_VALUE(MRES_IGNORED, TRUE);
+}
+
+void DLL_POST_ClientPutInServer(edict_t *pEntity) {
+    gLogPlayer.Update(pEntity);
+    gLogEvent.ClientPutInServer(pEntity);
+    RETURN_META(MRES_IGNORED);
+}
+
+void DLL_POST_ClientKill(edict_t *pEntity) {
+    gLogPlayer.Update(pEntity);
+    gLogEvent.ClientKill(pEntity);
+    RETURN_META(MRES_IGNORED);
+}
+
+void DLL_POST_ClientUserInfoChanged(edict_t *pEntity, char *InfoBuffer) {
+    gLogPlayer.Update(pEntity);
+    gLogEvent.ClientUserInfoChanged(pEntity, InfoBuffer);
+    RETURN_META(MRES_IGNORED);
+}
+
+void DLL_POST_ClientCommand(edict_t *pEntity) {
+    if (pEntity && !FNullEnt(pEntity)) {
+        gLogPlayer.Update(pEntity);
+        gLogEvent.ClientCommand(pEntity);
+        gLogEvent.ClientSay(pEntity);
+    }
+    RETURN_META(MRES_IGNORED);
+}
+
+// ✅ ENGINE_POST Function Definition
+void ENGINE_POST_AlertMessage(ALERT_TYPE atype, const char *szFmt, ...) {
+    if (atype == at_logged) {
+        static char szBuffer[1024];
+        va_list vArgList;
+        va_start(vArgList, szFmt);
+        vsnprintf(szBuffer, sizeof(szBuffer), szFmt, vArgList);
+        va_end(vArgList);
+
+        if (szBuffer[0u] != '\0') {
+            gLogEvent.ServerAlertMessage(atype, szBuffer);
+        }
+    }
+    RETURN_META(MRES_IGNORED);
+}
+
 #pragma endregion
-
-// ✅ DLL_POST Functions (Must be defined somewhere in the project)
-void DLL_POST_ServerActivate(edict_t *pEdictList, int edictCount, int clientMax);
-void DLL_POST_ServerDeactivate(void);
-void DLL_POST_StartFrame(void);
-qboolean DLL_POST_ClientConnect(edict_t *pEntity, const char *pszName, const char *pszAddress, char szRejectReason[128]);
-void DLL_POST_ClientPutInServer(edict_t *pEntity);
-void DLL_POST_ClientKill(edict_t *pEntity);
-void DLL_POST_ClientUserInfoChanged(edict_t *pEntity, char *InfoBuffer);
-void DLL_POST_ClientCommand(edict_t *pEntity);
-
-// ✅ ENGINE_POST Functions
-void ENGINE_POST_AlertMessage(ALERT_TYPE atype, const char *szFmt, ...);
